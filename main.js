e18565f1428a62024ec6a6ba98357b3bcc00a63c
IguhSoares <iguhsoares@gmail.com>
2022-11-03 11:55:37 -0300

    feat: init animations passing no options, using default values

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import initAnimations from './toggle.js';
 
-initAnimations('expand-collapse', {
-  duration: {
-    toggleHeight: { collapse: 500 },
-    total: { expand: 800, collapse: 1000 },
-  },
-});
-// initAnimations('expand-collapse');
+// initAnimations('expand-collapse', {
+//   duration: {
+//     toggleHeight: { collapse: 500 },
+//     total: { expand: 800, collapse: 1000 },
+//   },
+// });
+initAnimations('expand-collapse');
